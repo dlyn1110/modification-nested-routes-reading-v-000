537470026f24f2e1b2049b30170b7219079c68d0
Dani Lyn <dlyn1110@gmail.com>
2019-01-08 19:54:50 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   resources :authors, only: [:show, :index] do
+    
+  # this gives us access to /authors/:id/posts/new, and a new_author_post_path helper.
     resources :posts, only: [:show, :index,:new]
   end
 
